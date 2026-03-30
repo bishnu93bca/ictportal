5d3e8cc10bbcd4aba6ab164f2d84549e37e6184f
Bishnu Ray <bishnu99bca@gmail.com>
2026-03-31 00:02:39 +0530

    react

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🚀 Laravel + React is Working!</h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
